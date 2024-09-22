593ca71a2527f4fba9f74108287f30580f16e4bd
syrac <carys.cooper@univ.ox.ac.uk>
2024-09-22 23:37:35 +0100

    add function add_alarm

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ def update_time():
     time_label.config(text=now)
     root.after(1000, update_time)
 
+def add_alarm(hour, minute, sound):
+    alarms.append({"hour": hour, "minute": minute, "sound": sound})
+
 root = tk.Tk()
 root.title("Pi-o-Clock")
 root.geometry("480x320")
+
+alarms = []  # List to store alarm times and sounds
 
 time_label = tk.Label(root, text="", font=("Helvetica", 48))
 time_label.pack(expand=True)
